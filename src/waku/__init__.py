452c6979f71683c2d56d70f4505f0df86738a756
Daniil Kharkov <fadeddexofan@gmail.com>
2024-12-20 11:54:38 +0300

    chore(deps): actualize pyproject and version in init

--- src/waku/__init__.py
+++ src/waku/__init__.py
@@ -1,9 +1,10 @@
 from waku.application import Application, ApplicationLifespan
 from waku.module import Module
 
-__version__ = 'v0.1.0'
+__version__ = 'v0.1.2'
 __all__ = [
     'Application',
     'ApplicationLifespan',
     'Module',
+    '__version__',
 ]
