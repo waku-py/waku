7a270d4929d97d06a0482e85680215e38672b39f
Daniil Kharkov <fadeddexofan@gmail.com>
2024-12-20 14:49:35 +0300

    fix: remove unused version var from init, run gitlint only for prs

--- src/waku/__init__.py
+++ src/waku/__init__.py
@@ -1,10 +1,8 @@
 from waku.application import Application, ApplicationLifespan
 from waku.module import Module
 
-__version__ = 'v0.1.2'
 __all__ = [
     'Application',
     'ApplicationLifespan',
     'Module',
-    '__version__',
 ]
