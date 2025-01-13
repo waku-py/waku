35212449029965265e367ff94b6c642930adee19
Daniil Kharkov <fadeddexofan@gmail.com>
2025-01-13 20:39:53 +0300

    feat: refactor module system

---
+++ src/waku/modules/__init__.py
@@ -0,0 +1,11 @@
+from waku.modules._metadata import DynamicModule, ModuleCompiler, ModuleMetadata, ModuleType, module
+from waku.modules._module import Module
+
+__all__ = [
+    'DynamicModule',
+    'Module',
+    'ModuleCompiler',
+    'ModuleMetadata',
+    'ModuleType',
+    'module',
+]
