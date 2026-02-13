25ea5abb8bb8a5f3185d4158403b64eef08522e8
Daniil Kharkov <fadeddexofan@gmail.com>
2026-02-13 18:48:06 +0700

    feat(ext): add UpcasterChain with sparse chain model

--- src/waku/eventsourcing/upcasting/__init__.py
+++ src/waku/eventsourcing/upcasting/__init__.py
@@ -1,7 +1,15 @@
+from waku.eventsourcing.upcasting.chain import UpcasterChain
 from waku.eventsourcing.upcasting.fn import FnUpcaster
+from waku.eventsourcing.upcasting.helpers import add_field, noop, remove_field, rename_field, upcast
 from waku.eventsourcing.upcasting.interfaces import IEventUpcaster
 
 __all__ = [
     'FnUpcaster',
     'IEventUpcaster',
+    'UpcasterChain',
+    'add_field',
+    'noop',
+    'remove_field',
+    'rename_field',
+    'upcast',
 ]
